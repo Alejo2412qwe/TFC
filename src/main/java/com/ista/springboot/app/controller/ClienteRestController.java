9f496ce4538c6990c7e7a653bc80f360e415c757
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-03-12 10:56:00 -0500

    update

--- src/main/java/com/ista/springboot/app/controller/ClienteRestController.java
+++ src/main/java/com/ista/springboot/app/controller/ClienteRestController.java
@@ -17,8 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ista.springboot.app.models.entity.Cliente;
 import com.ista.springboot.app.models.services.IClienteService;
-
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
